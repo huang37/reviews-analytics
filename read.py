4fd97d02df202004f0b44b1e61e0d79ea96d02fa
Huang <huang37@mail2000.com.tw>
2018-03-23 21:12:03 +0800

    新增文字計數的部份

--- read.py
+++ read.py
@@ -9,16 +9,60 @@ with open('reviews.txt', 'r') as f:
 
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
-sum_len = 0
-for d in data:
-  sum_len += len(d)
-  print(sum_len)
+print(data[0])
 
-print('留言的平均長度是', sum_len/len(data))
-
-new = []
+wc = {}
 for d in data:
-  if len(d) < 100:
-    new.append(d)
-print('一共有', len(new), '筆的留言長度小於100')
-print(new[0])
+  words = d.split()
+  for word in words:
+    if word in wc:
+      wc[word] += 1
+    else:
+      wc[word] = 1 #新增新的key進字典
+for word in wc:
+  if wc[word] > 100:
+    print(word, wc[word])
+print(len(wc))
+  
+while  True:
+  word = input('請問你想查什麼字：')
+  if word == 'q':
+    break
+  if word in wc:
+    print(word, '出現過的次數為：',wc[word])
+  else:
+    print('這個字沒有出現過喔！！！')
+print('感謝使用')
+
+
+
+
+
+
+
+# sum_len = 0
+# for d in data:
+#   sum_len += len(d)
+#   print(sum_len)
+
+# print('留言的平均長度是', sum_len/len(data))
+
+# new = []
+# for d in data:
+#   if len(d) < 100:
+#     new.append(d)
+# print('一共有', len(new), '筆的留言長度小於100')
+# print(new[0])
+
+
+# good = []
+# for d in data:
+#   if 'good' in d:
+#     good.append(d)
+# print('一共有', len(good), '筆的留言')
+# print(good[0])
+
+
+
+
+
